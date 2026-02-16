e4d7d1e719c2968c2f628fa3fc35e20915b53d2d
Garima narain <garimanarain85@gmail.com>
2026-02-16 20:08:56 +0530

    UC1: Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * Program Name : OOPSBannerApp
+ * Description  : Display OOPS in Banner Format
+ * Author       : Garima Narain
+ * Version      : 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+
